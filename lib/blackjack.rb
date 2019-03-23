b52d9267317f0503e836c2e49dd870fbc98f7fe7
John Hill <john.m.hill91@gmail.com>
2019-03-23 03:11:06 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -7,11 +7,11 @@ def deal_card
 end
 
 def display_card_total(total)
-  puts "Your cards add up to #{total}"
+  puts "Your cards add up to #{total}."
 end
 
 def prompt_user
-  puts "Type 'hit' for hit or 'stay' to stay"
+  puts "Type 'hit' for hit or 'stay' to stay."
 end
 
 def get_user_input
